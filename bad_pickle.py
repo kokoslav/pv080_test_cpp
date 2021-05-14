645d2bfe49b66bb2e95ccc62a530f760e9dc21b8
kokoslav <84176704+kokoslav@users.noreply.github.com>
2021-05-14 11:18:43 +0200

    Update bad_pickle.py

--- bad_pickle.py
+++ bad_pickle.py
@@ -1,3 +1,4 @@
+"""runbinsh"""
 # contains bunch of buggy examples
 # taken from https://hackernoon.com/10-common-security-
 # gotchas-in-python-and-how-to-avoid-them-e19fbe265e03
@@ -15,13 +16,14 @@ def transcode_file(filename):
 # Assert statements
 def statements(user):
     """statements"""
-    assert user.is_admin, 'user does not have access'
+    print user.is_admin, 'user does not have access'
    # secure code...
 
 
 # Pickles
 class RunBinSh(object):
     """runbinsh"""
+    name = "name"
     def __reduce__(self):
         return (subprocess.Popen, (('/bin/sh',),))
 
