67e51c9357fb69f566a6f3fad576f3edcc77cd99
kokoslav <84176704+kokoslav@users.noreply.github.com>
2021-05-14 11:08:51 +0200

    Update bad_pickle.py

--- bad_pickle.py
+++ bad_pickle.py
@@ -7,18 +7,21 @@ import subprocess
 
 # Input injection
 def transcode_file(filename):
+    """transcode_file"""
     command = 'ffmpeg -i "{source}" output_file.mpg'.format(source=filename)
     subprocess.call(command, shell=True)  # a bad idea!
 
 
 # Assert statements
 def statements(user):
+    """statements"""
     assert user.is_admin, 'user does not have access'
    # secure code...
 
 
 # Pickles
 class RunBinSh(object):
+    """runbinsh"""
     def __reduce__(self):
         return (subprocess.Popen, (('/bin/sh',),))
 
